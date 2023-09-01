8c2075db9733b698bd423ba470e43e182b8edece
Ronaldo Silva <ronaldosdea@gmail.com>
2023-09-01 11:44:00 -0300

    atualizando repositorio

---
+++ html/Atividade 20/script.js
@@ -0,0 +1,18 @@
+const titulo = window.document.getElementsByTagName('h1')
+
+
+const divs = window.document.getElementsByClassName('quadrado')
+const redonda = document.getElementsByClassName('circulo')
+
+ 
+// querySelector
+// querySelectorAll
+
+const lista = document.querySelectorAll('.quadrado')
+
+
+console.log()
+
+// console.log(titulo)
+// console.log(divs)
+// console.log(redonda)
